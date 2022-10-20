19808f3d27b6eb502dafa96183d6584d25c413a8
18074kosen <18074@hakodate.kosen-ac.jp>
2022-10-20 10:07:23 +0900

    Bがキャラ追加

--- game6/aaaa.java
+++ game6/aaaa.java
@@ -1,19 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class techi here.
+ * Write a description of class aaaa here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class techi extends Actor
+public class aaaa extends Actor
 {
     /**
-     * Act - do whatever the techi wants to do. This method is called whenever
+     * Act - do whatever the aaaa wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
+    public void act() 
     {
         // Add your action code here.
-    }
+    }    
 }
