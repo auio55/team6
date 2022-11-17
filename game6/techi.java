fb9d723374eaf8e282f458556ab7abc060e8037f
auio55 <eternityheart0423@gmail.com>
2022-11-17 10:05:47 +0900

    Aがキャラ移動処理を追加

--- game6/techi.java
+++ game6/techi.java
@@ -15,5 +15,9 @@ public class techi extends Actor
     public void act()
     {
         // Add your action code here.
+        if( Greenfoot.isKeyDown( "up" ) ){
+        setRotation(-90);
+        move(1);
+        }   
     }
 }
