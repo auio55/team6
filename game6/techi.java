ac511f9236deef88c1d0f9cf6706d1754b8cbc49
mmmiiiyu <18077@hakodate.kosen-ac.jp>
2022-11-24 09:23:11 +0900

    cがキャラ追加

--- game6/techi.java
+++ game6/techi.java
@@ -14,6 +14,9 @@ public class techi extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "down" ) ){
+        setRotation(90);
+        move(1);
+}// Add your action code here.
     }
 }
